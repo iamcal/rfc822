fb1c5288b3604a335f004e01f717a0d64eeeeb3c
Cal Henderson <cal@iamcal.com>
2009-02-20 20:32:45 +0000

    lots of improvements - check lengths of local and domain part. validate domain-literals for IPv4 and IPv6
    git-svn-id: https://svn.iamcal.com/public/php/rfc822/trunk@2709 ac7c8259-0de0-4f3c-bb6a-12394caf8efd

--- rfc3696.php
+++ rfc3696.php
@@ -30,7 +30,7 @@
 		$digit		= "[\\x30-\\x39]";
 		$cr		= "\\x0d";
 		$lf		= "\\x0a";
-		$crlf		= "($cr$lf)";
+		$crlf		= "(?:$cr$lf)";
 
 
 		####################################################################################
@@ -47,10 +47,10 @@
 		# quoted-pair     =       ("\" text) / obs-qp
 
 		$obs_char	= "[\\x00-\\x09\\x0b\\x0c\\x0e-\\x7f]";
-		$obs_text	= "($lf*$cr*($obs_char$lf*$cr*)*)";
-		$text		= "([\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f]|$obs_text)";
-		$obs_qp		= "(\\x5c[\\x00-\\x7f])";
-		$quoted_pair	= "(\\x5c$text|$obs_qp)";
+		$obs_text	= "(?:$lf*$cr*(?:$obs_char$lf*$cr*)*)";
+		$text		= "(?:[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f]|$obs_text)";
+		$obs_qp		= "(?:\\x5c[\\x00-\\x7f])";
+		$quoted_pair	= "(?:\\x5c$text|$obs_qp)";
 
 
 		####################################################################################
@@ -72,12 +72,12 @@
 		#
 
 		$wsp		= "[\\x20\\x09]";
-		$obs_fws	= "($wsp+($crlf$wsp+)*)";
-		$fws		= "((($wsp*$crlf)?$wsp+)|$obs_fws)";
-		$ctext		= "($no_ws_ctl|[\\x21-\\x27\\x2A-\\x5b\\x5d-\\x7e])";
-		$ccontent	= "($ctext|$quoted_pair)";
-		$comment	= "(\\x28($fws?$ccontent)*$fws?\\x29)";
-		$cfws		= "(($fws?$comment)*($fws?$comment|$fws))";
+		$obs_fws	= "(?:$wsp+(?:$crlf$wsp+)*)";
+		$fws		= "(?:(?:(?:$wsp*$crlf)?$wsp+)|$obs_fws)";
+		$ctext		= "(?:$no_ws_ctl|[\\x21-\\x27\\x2A-\\x5b\\x5d-\\x7e])";
+		$ccontent	= "(?:$ctext|$quoted_pair)";
+		$comment	= "(?:\\x28(?:$fws?$ccontent)*$fws?\\x29)";
+		$cfws		= "(?:(?:$fws?$comment)*(?:$fws?$comment|$fws))";
 		$cfws		= "$fws*";
 
 
@@ -96,8 +96,8 @@
 		#                         "~"
 		# atom            =       [CFWS] 1*atext [CFWS]
 
-		$atext		= "($alpha|$digit|[\\x21\\x23-\\x27\\x2a\\x2b\\x2d\\x2f\\x3d\\x3f\\x5e\\x5f\\x60\\x7b-\\x7e])";
-		$atom		= "($cfws?$atext+$cfws?)";
+		$atext		= "(?:$alpha|$digit|[\\x21\\x23-\\x27\\x2a\\x2b\\x2d\\x2f\\x3d\\x3f\\x5e\\x5f\\x60\\x7b-\\x7e])";
+		$atom		= "(?:$cfws?(?:$atext|$quoted_pair)+$cfws?)";
 
 
 		####################################################################################
@@ -112,10 +112,10 @@
 		#                         [CFWS]
 		# word            =       atom / quoted-string
 
-		$qtext		= "($no_ws_ctl|[\\x21\\x23-\\x5b\\x5d-\\x7e])";
-		$qcontent	= "($qtext|$quoted_pair)";
-		$quoted_string	= "($cfws?\\x22($fws?$qcontent)*$fws?\\x22$cfws?)";
-		$word		= "($atom|$quoted_string)";
+		$qtext		= "(?:$no_ws_ctl|[\\x21\\x23-\\x5b\\x5d-\\x7e])";
+		$qcontent	= "(?:$qtext|$quoted_pair)";
+		$quoted_string	= "(?:$cfws?\\x22(?:$fws?$qcontent)*$fws?\\x22$cfws?)";
+		$word		= "(?:$atom|$quoted_string)";
 
 
 		####################################################################################
@@ -123,8 +123,8 @@
 		# obs-local-part  =       word *("." word)
 		# obs-domain      =       atom *("." atom)
 
-		$obs_local_part	= "($word(\\x2e$word)*)";
-		$obs_domain	= "($atom(\\x2e$atom)*)";
+		$obs_local_part	= "(?:$word(?:\\x2e$word)*)";
+		$obs_domain	= "(?:$atom(?:\\x2e$atom)*)";
 
 
 		####################################################################################
@@ -132,8 +132,8 @@
 		# dot-atom-text   =       1*atext *("." 1*atext)
 		# dot-atom        =       [CFWS] dot-atom-text [CFWS]
 
-		$dot_atom_text	= "($atext+(\\x2e$atext+)*)";
-		$dot_atom	= "($cfws?$dot_atom_text$cfws?)";
+		$dot_atom_text	= "(?:$atext+(?:\\x2e$atext+)*)";
+		$dot_atom	= "(?:$cfws?$dot_atom_text$cfws?)";
 
 
 		####################################################################################
@@ -146,9 +146,9 @@
 		#                         %d94-126        ;  characters not including "[",
 		#                                         ;  "]", or "\"
 
-		$dtext		= "($no_ws_ctl|[\\x21-\\x5a\\x5e-\\x7e])";
-		$dcontent	= "($dtext|$quoted_pair)";
-		$domain_literal	= "($cfws?\\x5b($fws?$dcontent)*$fws?\\x5d$cfws?)";
+		$dtext		= "(?:$no_ws_ctl|[\\x21-\\x5a\\x5e-\\x7e])";
+		$dcontent	= "(?:$dtext|$quoted_pair)";
+		$domain_literal	= "(?:$cfws?\\x5b(?:$fws?$dcontent)*$fws?\\x5d$cfws?)";
 
 
 		####################################################################################
@@ -157,10 +157,9 @@
 		# domain          =       dot-atom / domain-literal / obs-domain
 		# addr-spec       =       local-part "@" domain
 
-		$local_part	= "($dot_atom|$quoted_string|$obs_local_part)";
-		$domain		= "($dot_atom|$domain_literal|$obs_domain)";
-		$domain		= "$dot_atom";
-		$addr_spec	= "($local_part\\x40$domain)";
+		$local_part	= "(($dot_atom)|($quoted_string)|($obs_local_part))";
+		$domain		= "(($dot_atom)|($domain_literal)|($obs_domain))";
+		$addr_spec	= "$local_part\\x40$domain";
 
 
 		#
@@ -182,7 +181,112 @@
 		# now match what's left
 		#
 
-		return preg_match("!^$addr_spec$!", $email) ? 1 : 0;
+		if (!preg_match("!^$addr_spec$!", $email, $m)){
+
+			return 0;
+		}
+
+		$bits = array(
+			'local'			=> $m[1],
+			'local-atom'		=> $m[2],
+			'local-quoted'		=> $m[3],
+			'local-obs'		=> $m[4],
+			'domain'		=> $m[5],
+			'domain-atom'		=> $m[6],
+			'domain-literal'	=> $m[7],
+			'domain-obs'		=> $m[8],
+		);
+
+#var_export($bits); echo "\n";
+
+
+		#
+		# length limits on segments
+		#
+
+		if (strlen($bits[local]) > 64) return 0;
+		if (strlen($bits[domain]) > 255) return 0;
+
+
+		#
+		# restrictuions on domain-literals from RFC2821 section 4.1.3
+		#
+
+		if (strlen($bits['domain-literal'])){
+
+			$Snum			= "(\d{1,3})";
+			$IPv4_address_literal	= "$Snum\.$Snum\.$Snum\.$Snum";
+
+			$IPv6_hex		= "(?:[0-9a-fA-F]{1,4})";
+
+			$IPv6_full		= "IPv6\:$IPv6_hex(:?\:$IPv6_hex){7}";
+
+			$IPv6_comp_part		= "(?:$IPv6_hex(?:\:$IPv6_hex){0,5})?";
+			$IPv6_comp		= "IPv6\:($IPv6_comp_part\:\:$IPv6_comp_part)";
+
+			$IPv6v4_full		= "IPv6\:$IPv6_hex(?:\:$IPv6_hex){5}\:$IPv4_address_literal";
+
+			$IPv6v4_comp_part	= "$IPv6_hex(?:\:$IPv6_hex){0,3}";
+			$IPv6v4_comp		= "IPv6\:((?:$IPv6v4_comp_part)?\:\:(?:$IPv6v4_comp_part\:)?)$IPv4_address_literal";
+
+
+			#
+			# IPv4 is simple
+			#
+
+			if (preg_match("!^\[$IPv4_address_literal\]$!", $bits[domain], $m)){
+
+				if (intval($m[1]) > 255) return 0;
+				if (intval($m[2]) > 255) return 0;
+				if (intval($m[3]) > 255) return 0;
+				if (intval($m[4]) > 255) return 0;
+
+			}else{
+
+				#
+				# this should be IPv6 - a bunch of tests are needed here :)
+				#
+
+				while (1){
+
+					if (preg_match("!^\[$IPv6_full\]$!", $bits[domain])){
+						break;
+					}
+
+					if (preg_match("!^\[$IPv6_comp\]$!", $bits[domain], $m)){
+						list($a, $b) = explode('::', $m[1]);
+						$folded = (strlen($a) && strlen($b)) ? "$a:$b" : "$a$b";
+						$groups = explode(':', $folded);
+						if (count($groups) > 6) return 0;
+						break;
+					}
+
+					if (preg_match("!^\[$IPv6v4_full\]$!", $bits[domain], $m)){
+
+						if (intval($m[1]) > 255) return 0;
+						if (intval($m[2]) > 255) return 0;
+						if (intval($m[3]) > 255) return 0;
+						if (intval($m[4]) > 255) return 0;
+						break;
+					}
+
+					if (preg_match("!^\[$IPv6v4_comp\]$!", $bits[domain], $m)){
+						list($a, $b) = explode('::', $m[1]);
+						$b = substr($b, 0, -1); # remove the trailing colon before the IPv4 address
+						$folded = (strlen($a) && strlen($b)) ? "$a:$b" : "$a$b";
+						$groups = explode(':', $folded);
+						if (count($groups) > 4) return 0;
+						break;
+					}
+
+					return 0;
+				}
+			}			
+		}
+
+
+
+		return 1;
 	}
 
 	##################################################################################
